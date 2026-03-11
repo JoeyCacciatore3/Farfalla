ab6bc41dcb0761f5aecdbcf78d6c3b2d065b79ea
Joey <joey@example.com>
2026-03-11 09:22:35 -0400

    Configure cache-busting headers and file hashes

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ command }) => ({
   base: command === 'build' ? '/Farfalla/' : '/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash].js`,
+        chunkFileNames: `assets/[name]-[hash].js`,
+        assetFileNames: `assets/[name]-[hash].[ext]`,
+      },
+    },
+  },
 }))
